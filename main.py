86148e1f7ceb309f2df6959c4fd3c5e596aedc28
Jhon Sebastián Londoño <jhonselo2000@hotmail.com>
2022-08-21 22:42:36 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+
+# Jhon Sebastián Londoño Cárdenas.
+
+from email import header
+from typing import Union
+from urllib import request, response
+from fastapi import FastAPI
+import requests
+from pydantic import BaseModel
+
+app = FastAPI()
+URL = 'https://6302c686c6dda4f287bdba1c.mockapi.io/items'
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+# Métodos GET
+
+@app.get("/items")
+def read_root():
+    response = requests.get(URL, {}, timeout=5)
+    return {"items": response.json()}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+# Método PUT
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    response = requests.put(URL+"/"+str(item_id), item.json(), headers = headers)
+    return response.json()
+
+# Método DELETE    
+
+@app.delete("/items/{item_id}")
+def remove_item(item_id: int):
+    response = requests.delete(URL+"/"+str(item_id))
+    return response.json()
+
+# Método POST
+
+@app.post("/items")
+def create_item(item: Item):
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    response = requests.post(URL, item.json(), headers = headers)
+    return response.json()
